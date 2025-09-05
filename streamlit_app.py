2fd82dae023e47ad594cb4919a5c866623bade7f
IIDA113 <Takuya11.Iida@jp.nttdata.com>
2025-09-05 02:48:05 +0000

    urlka

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,4 +50,4 @@ else:
         with st.chat_message("assistant"):
             st.markdown(assistant_reply)
             if image_url:
-                st.image(image_url)
+                st.test(image_url)
